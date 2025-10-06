bf872ac8fab522254062c4c0d74cd940ce7336b6
Rails Designer <devs@railsdesigner.com>
2025-10-06 15:18:04 +0700

    Sortable between columns

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,11 @@
 class CardsController < ApplicationController
   def update
-    Board::Card.find(params[:id]).update position: new_position
+    Board::Card.find(params[:id]).update board_column_id: board_column_id, position: new_position
   end
 
   private
+
+  def board_column_id = params[:new_list_id]
 
   def new_position = params[:new_position]
 end
--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -3,11 +3,12 @@ import Sortable from "sortablejs"
 import { patch } from "@rails/request.js"
 
 export default class extends Controller {
-  static values = { endpoint: String };
+  static values = { groupName: String, endpoint: String };
 
   connect() {
     Sortable.create(this.element,
       {
+        group: this.groupNameValue,
         draggable: "[draggable]",
         animation: 250,
         easing: "cubic-bezier(1, 0, 0, 1)",
@@ -23,8 +24,8 @@ export default class extends Controller {
 
   async #updatePosition(event) {
     await patch(
-      this.endpointValue.replace(/__ID__/g, event.item.dataset.sortableIdValue),
-      { body: JSON.stringify({ new_position: event.newIndex + 1 }) }
+      this.endpointValue.replace(/__ID__/g, event.item.dataset.sortableIdValue), // the `sortableIdValue` works for both columns and cards
+      { body: JSON.stringify({ new_list_id: event.to.dataset.sortableListIdValue, new_position: event.newIndex + 1 }) }
     )
   }
 }
